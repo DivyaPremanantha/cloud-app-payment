9697620acc68ad08bd48b4d9c46371816f5fd021
DivyaPremanantha <premananthadivya@gmail.com>
2020-12-30 11:42:42 +0530

    Add initial commit

---
+++ payment/app.js
@@ -0,0 +1,80 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+const databaseManager = require('./databaseManager');
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+exports.paymentHandler = async (event) => {
+	console.log(event);
+
+	switch (event.httpMethod) {
+		case 'DELETE':
+			return deletePayment(event);
+		case 'GET':
+			return getPayment(event);
+		case 'POST':
+			return savePayment(event);
+		case 'PUT':
+			return updatePayment(event);
+		default:
+			return sendResponse(404, `Unsupported method "${event.httpMethod}"`);
+	}
+};
+
+function savePayment(event) {
+	const payment = JSON.parse(event.body);
+	payment.paymentId = Math.floor(100000 + Math.random() * 900000).toString();
+
+	return databaseManager.savePayment(payment).then(response => {
+		console.log(response);
+		return sendResponse(200, payment.paymentId);
+	});
+}
+
+function getPayment(event) {
+	const paymentId = event.pathParameters.paymentId;
+
+	return databaseManager.getPayment(paymentId).then(response => {
+		console.log(response);
+		return sendResponse(200, JSON.stringify(response));
+	});
+}
+
+function deletePayment(event) {
+	const paymentId = event.pathParameters.paymentId;
+
+	return databaseManager.deletePayment(paymentId).then(response => {
+		return sendResponse(200, 'DELETE payment');
+	});
+}
+
+function updatePayment(event) {
+	const paymentId = event.pathParameters.paymentId;
+
+	const body = JSON.parse(event.body);
+	const paramName = body.paramName;
+	const paramValue = body.paramValue;
+
+	return databaseManager.updatePayment(paymentId, paramName, paramValue).then(response => {
+		console.log(response);
+		return sendResponse(200, JSON.stringify(response));
+	});
+}
+
+function sendResponse(statusCode, message) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	return response
+}
---
+++ payment/databaseManager.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_NAME = process.env.TABLE_NAME;
+
+module.exports.initializateDynamoClient = newDynamo => {
+	dynamo = newDynamo;
+};
+
+module.exports.savePayment = payment => {
+	const params = {
+		TableName: TABLE_NAME,
+		Item: payment
+	};
+
+	return dynamo
+		.put(params)
+		.promise()
+		.then(() => {
+			return payment.paymentId;
+		});
+};
+
+module.exports.getPayment = paymentId => {
+	const params = {
+		Key: {
+			paymentId: paymentId
+		},
+		TableName: TABLE_NAME
+	};
+
+	return dynamo
+		.get(params)
+		.promise()
+		.then(result => {
+			return result.payment;
+		});
+};
+
+module.exports.deletePayment = paymentId => {
+	const params = {
+		Key: {
+			paymentId: paymentId
+		},
+		TableName: TABLE_NAME
+	};
+
+	return dynamo.delete(params).promise();
+};
+
+module.exports.updatePayment = (paymentId, paramsName, paramsValue) => {
+	const params = {
+		TableName: TABLE_NAME,
+		Key: {
+			paymentId
+		},
+		ConditionExpression: 'attribute_exists(paymentId)',
+		UpdateExpression: 'set ' + paramsName + ' = :v',
+		ExpressionAttributeValues: {
+			':v': paramsValue
+		},
+		ReturnValues: 'ALL_NEW'
+	};
+
+	return dynamo
+		.update(params)
+		.promise()
+		.then(response => {
+			return response.Attributes;
+		});
+};
